701bc957ea81b131a1ad2f490102c425da7902b7
Serj Pyshkin <IndianaCornell@gmail.com>
2024-09-29 18:34:42 +0300

    done?

--- js/burger.js
+++ js/burger.js
@@ -1,15 +1,15 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-burger-open]"),
-      closeModalBtn: document.querySelector("[data-burger-close]"),
-      modal: document.querySelector("[data-burger]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-      refs.modal.classList.toggle("is-open");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
--- js/modal.js
+++ js/modal.js
@@ -9,7 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
     refs.modal.classList.toggle("is-open");
+    document.body.classList.toggle('no-scroll');
   }
 })();
